2361696681f38e36fe6f2fc0df595ab794cd71d3
aqulline <aqeglipambuya@gmail.com>
2022-10-18 01:19:58 +0300

    Now i know what i am doing
    psych

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.properties import NumericProperty
 from kivymd.app import MDApp
 from kivy.core.window import Window
 
@@ -7,8 +8,16 @@ Window.minimum_width, Window.minimum_height = Window.size
 
 
 class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.size_y = NumericProperty(0)
+        self.size_x = NumericProperty(0)
+
     def build(self):
-        print(Window.size)
+        self.size_x, self.size_y = Window.size
+        self.theme_cls.theme_style = "Light"
+        self.size_x, self.size_y = Window.size
+        self.title = "POCKET"
 
 
 MainApp().run()
