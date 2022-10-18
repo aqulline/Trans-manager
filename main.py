ca42cdbd03be8f71122cfa5219fbc1c0a32274bc
aqulline <aqeglipambuya@gmail.com>
2022-10-19 01:37:48 +0300

    add a lot of parameters
    listening to chris brown

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivymd_extensions import akivymd
 
 Window.size = (1920, 1016)
 Window.minimum_width, Window.minimum_height = Window.size
 
 
 class MainApp(MDApp):
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.size_y = NumericProperty(0)
-        self.size_x = NumericProperty(0)
+    # App
+    size_x, size_y = NumericProperty(0), NumericProperty(0)
+
+    # FLEET
+    car_name = StringProperty("")
+    car_id = StringProperty("")
+    car_year_purchased = StringProperty("")
+    car_owner = StringProperty("")
+    car_driver = StringProperty("")
+    car_status = StringProperty("")
+
+    # MAINTENANCE
+    date_reported = StringProperty("")
+    reported_issue = StringProperty("")
+    maintenance_status = StringProperty("")
+    maintenance_cost = StringProperty("")
+    total_maintenance = StringProperty("")
+
+    # FUEL-UTILIZATION
+    starting_km = StringProperty("")
+    ending_km = StringProperty("")
+    km_travel = StringProperty("")
+    consumption_km = StringProperty("")
+    fuel_given = StringProperty("")
+    total_given = StringProperty("")
+    fuel_used = StringProperty("")
+    total_used = StringProperty("")
+    fuel_price = StringProperty("")
+    fuel_amount = StringProperty("")
+    total_amount = StringProperty("")
+    fuel_type = StringProperty("")
+    petrol_price = StringProperty("")
+    diesel_price = StringProperty("")
+    total_petrol = StringProperty("")
+    total_diesel = StringProperty("")
 
     def build(self):
         self.size_x, self.size_y = Window.size
         self.theme_cls.theme_style = "Light"
         self.size_x, self.size_y = Window.size
-        self.title = "POCKET"
+        self.title = "Transport Officer"
 
 
 MainApp().run()
